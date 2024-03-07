8c1b0ce1b5c4a9e49926df8a1e64a81c75ad5081
marinaescriva <marinaescriva.24@gamil.com>
2024-03-07 12:42:05 +0100

    feature: first login and configure the token of the user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt  from "jsonwebtoken";
 
 
 export const register = async(req, res) => {  //no se tipa request ni response porq es javascript!
@@ -53,11 +54,78 @@ export const register = async(req, res) => {  //no se tipa request ni response p
     };
 
 
-export const login = (req, res) => {  //no se tipa request ni response porq es javascript!
-    res.status(200).json(
-        {
-            success: true,
-            message: "login user successfully"
+export const login = async (req, res) => {  //no se tipa request ni response porq es javascript!
+    try {
+        const email = req.body.email
+        const password = req.body.password
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "email and password are mandatories"
+            })
         }
-    )
+
+        const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+        if (!validEmail.test(email)) {
+            return res.status(400).json(
+                {
+                    success: false,
+                    message: "Email format is not valid"
+                }
+            )
+        }
+                //   |  DE MANERA TEMPORAL SE INCLUYE ANY AL NO PODER DELCARARSE USER EN LA VERIFICACION DE CONTRASEÑA
+        const user = await User.findOne(
+            {
+                email: email,
+            }
+        )
+
+        console.log(user);
+
+
+        if (!user) {
+            res.status(400).json({
+                success: false,
+                message: "Email or password invalid"
+            })
+        }
+
+        const isValidPassword = bcrypt.compareSync(password, user.password)
+
+        if (!isValidPassword) {
+            return res.status(400).json({
+                success: false,
+                message: "Email or password invalid"
+            })
+        }
+
+        const token = jwt.sign(
+			{
+				userId: user._id,
+				roleName: user.role,
+			},
+			process.env.JWT_SECRET,
+			{
+				expiresIn: "2h",
+			}
+		);
+
+   
+        res.status(200).json({
+            success: true,
+            message: "User logged succesfully",
+            token: token //MOSTRAMOS EL TOKEN DE MANERA TEMPORAL PARA PODER PROBAR CON ÉL OTRA FUNCIONALIDADES
+        })
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: "User cant be logged",
+            error: error
+        })
+    }
 };
