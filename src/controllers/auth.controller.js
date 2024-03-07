2d32012fd410f8b0ed516cb71312137dd173aba9
Marina <marinaescriva.24@gmail.com>
2024-03-07 10:56:47 +0100

    feature: add register in user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,56 @@
 
-export const register = (req, res) => {  //no se tipa request ni response porq es javascript!
-    res.status(201).json(
-        {
-            success: true,
-            message: "register user successfully"
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+
+export const register = async(req, res) => {  //no se tipa request ni response porq es javascript!
+
+    
+        try {
+          
+            const email = req.body.email
+            const password = req.body.password
+    
+    
+            if (password.length < 6 || password.length > 10) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Password must contain between 6 and 10 characters"
+                })
+            }
+    
+    
+            const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+            if (!validEmail.test(email)) {
+                return res.status(400).json(
+                    {
+                        success: false,
+                        message: "format email invalid"
+                    }
+                )
+            }
+            const passwordEncrypted = bcrypt.hashSync(password, 5)
+
+            const newUser = await User.create({
+                email: email,
+                password: passwordEncrypted
+            }
+            )
+            
+    
+            res.status(201).json({
+                success: true,
+                message: "User registered succesfully",
+                data: newUser
+            })
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: "User cant be registered",
+                error: error
+            })
         }
-    )
-}
+    };
 
 
 export const login = (req, res) => {  //no se tipa request ni response porq es javascript!
@@ -16,4 +60,4 @@ export const login = (req, res) => {  //no se tipa request ni response porq es j
             message: "login user successfully"
         }
     )
-}
+};
